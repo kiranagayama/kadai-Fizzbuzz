a2f835192301bb6adf48d0bd3f56a20a62a8f00e
kira.nagayama <kira20011025@gmail.com>
2022-08-18 11:54:41 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -3,12 +3,12 @@ num_max = 100
 (1..num_max).each do |num|
 def fizzbuzz(num)
  if num%3==0&&num%5==0
-  puts 'Fizz Buzz'
+  return'Fizz Buzz'
  elsif num%5==0
-  puts 'Buzz'
+  return'Buzz'
  elsif num%3==0
-  puts 'Fizz'
- else puts num
+ return'Fizz'
+ else return num
  end
 end 
 puts fizzbuzz(num)
